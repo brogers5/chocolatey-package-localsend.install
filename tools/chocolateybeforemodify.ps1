779599de6b38a2aa6cdf8617f2ac40505ab38631
brogers5 <6869577+brogers5@users.noreply.github.com>
2024-07-15 01:09:18 -0400

    Add beforeModify script to handle new behavior difference
    Unlike the legacy MSIX package, the EXE package will not forcibly terminate LocalSend if it's running.

---
+++ tools/chocolateybeforemodify.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$processName = 'localsend_app'
+$softwareName = 'LocalSend'
+$process = Get-Process -Name $processName -ErrorAction SilentlyContinue
+
+if ($process) {
+    Write-Warning "$softwareName is currently running, stopping it to prevent upgrade/uninstall from blocking..."
+    Stop-Process -InputObject $process -ErrorAction SilentlyContinue
+
+    Start-Sleep -Seconds 3
+
+    $process = Get-Process -Name $processName -ErrorAction SilentlyContinue
+    if ($process) {
+        Write-Warning "$softwareName is still running despite stop request, force stopping it..."
+        Stop-Process -InputObject $process -Force -ErrorAction SilentlyContinue
+    }
+
+    Write-Warning "If upgrading, $softwareName may need to be manually restarted upon completion"
+}
+else {
+    Write-Debug "No running $softwareName process instances were found"
+}
