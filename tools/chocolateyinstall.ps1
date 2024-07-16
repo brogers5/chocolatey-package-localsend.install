2df3cdb8402992d76c4207cc532a4f8da086a32a
brogers5 <6869577+brogers5@users.noreply.github.com>
2024-07-16 00:06:22 -0400

    Skip over legacy package check for Windows 7 compatibility

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,26 +1,30 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+Confirm-WinMinimumBuild -ReqBuild 7601
+
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 . $toolsDir\helpers.ps1
 
-$storeSignedPackage = Get-StoreSignedPackage
-if ($null -ne $storeSignedPackage) {
-    Write-Warning "A legacy store-signed version of LocalSend (v$($storeSignedPackage.Version)) was detected.
-    LocalSend has since migrated to a separate developer-signed package as it was removed from the Microsoft Store.
-    To prevent possible issues with package coexistence, please manually uninstall this version at your earliest convenience."
-}
-
-$developerSignedPackage = Get-LegacyDeveloperSignedPackage
-if ($null -ne $developerSignedPackage) {
-    Write-Warning "A legacy developer-signed version of LocalSend (v$($developerSignedPackage.Version)) was detected.
-    To prevent possible issues with installation coexistence, this version will be uninstalled."
-    Uninstall-LegacyDeveloperPackage
+#Only check for legacy packages on Windows 10+
+#These functions use cmdlets and/or parameters that did not exist prior to Server 2012 R2
+if ([Environment]::OSVersion.Version.Major -ge 10) {
+    $storeSignedPackage = Get-StoreSignedPackage
+    if ($null -ne $storeSignedPackage) {
+        Write-Warning "A legacy store-signed version of LocalSend (v$($storeSignedPackage.Version)) was detected.
+        LocalSend has since migrated to a separate developer-signed package as it was removed from the Microsoft Store.
+        To prevent possible issues with package coexistence, please manually uninstall this version at your earliest convenience."
+    }
+    
+    $developerSignedPackage = Get-LegacyDeveloperSignedPackage
+    if ($null -ne $developerSignedPackage) {
+        Write-Warning "A legacy developer-signed version of LocalSend (v$($developerSignedPackage.Version)) was detected.
+        To prevent possible issues with installation coexistence, this version will be uninstalled."
+        Uninstall-LegacyDeveloperPackage
+    }
 }
 
 $fileName = 'LocalSend-1.15.0-windows-x86-64.exe'
 $filePath = Join-Path -Path $toolsDir -ChildPath $fileName
-
-Confirm-WinMinimumBuild -ReqBuild 7601
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
